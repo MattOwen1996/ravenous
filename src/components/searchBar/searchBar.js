5ac6c5b84d948ff61814140851b9e6be5eb178bd
MattOwen1996 <1996mattowen@gmail.com>
2018-10-14 19:43:57 +0100

    Moving methods over using props to use this stateful components and then binding them in a constructor

--- src/components/searchBar/searchBar.js
+++ src/components/searchBar/searchBar.js
@@ -10,6 +10,9 @@ const sortByOptions = {
 class SearchBar extends React.Component {
   constructor(props) {
     super(props);
+    this.handleTermChange = this.handleTermChange.bind(this);
+    this.handleLocationChange = this.handleLocationChange.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
     this.state = {
       term: '',
       location: '',
@@ -44,6 +47,11 @@ class SearchBar extends React.Component {
     this.setState({location: event.target.value});
   }
 
+  handleSearch(event) {
+    this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div className="SearchBar">
@@ -53,11 +61,11 @@ class SearchBar extends React.Component {
           </ul>
         </div>
         <div className="SearchBar-fields">
-          <input placeholder="Search Businesses" />
-          <input placeholder="Where?" />
+          <input placeholder="Search Businesses" onChange={this.handleTermChange} />
+          <input placeholder="Where?" onChange={this.handleLocationChange} />
         </div>
         <div className="SearchBar-submit">
-          <a>Lets Go</a>
+          <a onClick={this.handleSearch}>Lets Go</a>
         </div>
       </div>
     );
