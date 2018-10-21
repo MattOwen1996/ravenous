1761d226875c7786971f81fe8e617740625c86d8
MattOwen1996 <1996mattowen@gmail.com>
2018-10-21 17:34:07 +0100

    Added Yelp Api

--- src/App.js
+++ src/App.js
@@ -3,34 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 import BusinessList from '../src/components/businessList/businessList';
 import SearchBar from '../src/components/searchBar/searchBar';
+import Yelp from '../src/util/Yelp';
 
 
-const business = {
-  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      businesses: []
+    };
 
-const businesses = [
-  business,
-  business,
-  business,
-  business,
-  business,
-  business
-];
-
-class App extends Component {
+    this.searchYelp = this.searchYelp.bind(this);
+  }
 
   searchYelp(term, location, sortBy) {
-    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+    Yelp.search(term, location, sortBy).then(businesses => {
+      this.setState({businesses: businesses});
+    })
   }
 
   render() {
@@ -38,7 +28,7 @@ class App extends Component {
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar searchYelp={this.searchYelp} />
-        <BusinessList businesses={businesses} />
+        <BusinessList businesses={this.state.businesses} />
       </div>
     );
   }
