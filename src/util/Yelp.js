cd78b4cc217a0e0aeb8374402a637ee955bd22de
MattOwen1996 <1996mattowen@gmail.com>
2018-10-21 14:31:22 +0100

    Adding Yelp Api

---
+++ src/util/Yelp.js
@@ -0,0 +1,7 @@
+const apiKey = 'WpDp6lVl_zEnwLuZmF58Oy_OTZ3xo90aGdTz5GfDH5sZKyMgFmpCkg_7n8iwvfT33mtIFHexCA7_vfWh25b2hEjwMOldkTxZoxaZ6vmb0dibuK37fKezp3e5FnzMW3Yx';
+
+const Yelp = {
+  search(term, location, sortBy) {
+    
+  }
+};
